9332111a1b175cc1bee24ecc5c1b55baf27ceda7
ManueDiaz <md307145@students.davenport.k12.ia.us>
2019-05-15 11:56:42 -0500

    chapter 6

--- src/DistanceFromAvrage.java
+++ src/DistanceFromAvrage.java
@@ -45,7 +45,6 @@ public class DistanceFromAvrage {
     }
 
 
-
     public static void displayFor(int[] number){
         for (int i = 0; i < number.length; i++) {
             System.out.println(number[i]);
